561b3525fbe5248b449feec55e1ec60894086378
dpatterson8583 <dpatterson8583@gmail.com>
2023-04-24 15:35:37 -0500

    Initial commit for Bank Encapsulation

--- BankEncapsulation/Program.cs
+++ BankEncapsulation/Program.cs
@@ -4,7 +4,15 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var account = new BankAccount();
+
+            account.Deposit(50);
+            account.Deposit(50);
+            Console.WriteLine($"Balance:  {account.GetBalance()}");
+
+            account.Deposit(20);
+            account.Deposit(350);
+            Console.WriteLine($"Balance:  {account.GetBalance()}");
         }
     }
 }
